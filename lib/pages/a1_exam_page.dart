b24bcfc29c311853337478a5e5cf475cc9c7df20
Ari <ari934318@gmail.com>
2026-09-13 18:52:04 +0330

    Update a1_exam_page.dart

--- lib/pages/a1_exam_page.dart
+++ lib/pages/a1_exam_page.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
+import '../data/levels/a1/a1_models.dart';
 
 import '../data/levels/a1/a1_exam_data.dart';
 import '../data/levels/a1/a1_exam_model.dart';
