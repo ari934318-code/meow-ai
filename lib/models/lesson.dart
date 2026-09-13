000f512fd74888ffe8f65fbefdae49efaec5b8e0
Ari <ari934318@gmail.com>
2026-09-13 07:19:56 +0330

    Update lesson.dart
    Expand lesson content model

--- lib/models/lesson.dart
+++ lib/models/lesson.dart
@@ -5,11 +5,42 @@ class Lesson {
   final String description;
   final int xp;
 
+  final List<LessonSection> sections;
+
   const Lesson({
     required this.id,
     required this.title,
     required this.level,
     required this.description,
     required this.xp,
+    this.sections = const [],
+  });
+}
+
+class LessonSection {
+  final String title;
+  final String type;
+  final String explanation;
+  final List<LessonItem> items;
+
+  const LessonSection({
+    required this.title,
+    required this.type,
+    this.explanation = '',
+    this.items = const [],
+  });
+}
+
+class LessonItem {
+  final String english;
+  final String persian;
+  final String example;
+  final String pronunciation;
+
+  const LessonItem({
+    required this.english,
+    required this.persian,
+    this.example = '',
+    this.pronunciation = '',
   });
 }
