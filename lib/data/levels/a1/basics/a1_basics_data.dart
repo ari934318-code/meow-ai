1642679ef3beb40c7e2ea95d4b150f0b6170d7a0
Ari <ari934318@gmail.com>
2026-09-13 23:28:29 +0330

    Create lib/data/levels/a1/basics/a1_basics_data.dart

---
+++ lib/data/levels/a1/basics/a1_basics_data.dart
@@ -0,0 +1,27 @@
+import 'a1_basics_models.dart';
+
+import 'a1_01_pronouns.dart';
+import 'a1_02_to_be.dart';
+import 'a1_03_have_has.dart';
+import 'a1_04_do_does.dart';
+import 'a1_05_regular_verbs.dart';
+import 'a1_06_irregular_verbs.dart';
+import 'a1_07_can_cant.dart';
+import 'a1_08_must_mustnt.dart';
+import 'a1_09_object_pronouns.dart';
+import 'a1_10_possessive_adjectives.dart';
+import 'a1_11_present_simple.dart';
+
+const List<A1BasicLesson> a1BasicsLessons = [
+  a1BasicPronouns,
+  a1BasicToBe,
+  a1BasicHaveHas,
+  a1BasicDoDoes,
+  a1BasicRegularVerbs,
+  a1BasicIrregularVerbs,
+  a1BasicCanCant,
+  a1BasicMustMustnt,
+  a1BasicObjectPronouns,
+  a1BasicPossessiveAdjectives,
+  a1BasicPresentSimple,
+];
