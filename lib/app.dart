c61bb08a6a27be0eee003b61bccd2722b20755f2
Ari <ari934318@gmail.com>
2026-09-13 09:40:57 +0330

    Update app.dart
    Fix Persian Material localization

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'localization.dart';
 
@@ -39,6 +40,9 @@ class MeowApp extends StatelessWidget {
           ],
           localizationsDelegates: const [
             MeowLocalizationsDelegate(),
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
           ],
           theme: ThemeData(
             brightness: Brightness.light,
