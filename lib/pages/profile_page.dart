fb8e8979a5bea70d4c418e531659c55a0ab397ea
Ari <ari934318@gmail.com>
2026-09-13 06:36:51 +0330

    Update profile_page.dart
    Update Settings page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -30,7 +30,7 @@ class _SettingsPageState extends State<SettingsPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Settings ⚙️',
+          'Settings',
           style: TextStyle(
             fontWeight: FontWeight.w700,
           ),
