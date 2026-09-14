016f70a1372f4ddc22da64d07fb2357d9db79660
Ari <ari934318@gmail.com>
2026-09-14 05:40:28 +0330

    Update a2_lesson_page.dart

--- lib/data/levels/a2/a2_lesson_page.dart
+++ lib/data/levels/a2/a2_lesson_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../localization.dart';
 import 'a2_data.dart';
