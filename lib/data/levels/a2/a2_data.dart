4e3e68b130ca9c96980309ff350628d6843a2689
Ari <ari934318@gmail.com>
2026-09-14 04:43:28 +0330

    Update a2_data.dart

--- lib/data/levels/a2/a2_data.dart
+++ lib/data/levels/a2/a2_data.dart
@@ -6,6 +6,7 @@ import 'a2_lesson_04.dart';
 import 'a2_lesson_05.dart';
 import 'a2_lesson_06.dart';
 import 'a2_lesson_07.dart';
+import 'a2_lesson_08.dart';
 
 const List<A2Lesson> a2Lessons = [
   a2Lesson01,
@@ -15,4 +16,5 @@ const List<A2Lesson> a2Lessons = [
   a2Lesson05,
   a2Lesson06,
   a2Lesson07,
+  a2Lesson08,
 ];
