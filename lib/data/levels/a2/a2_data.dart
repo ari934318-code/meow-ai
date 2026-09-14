ba4afb5a23b62f3dcc0cd05d3439d8074e619976
Ari <ari934318@gmail.com>
2026-09-14 04:33:46 +0330

    Update a2_data.dart

--- lib/data/levels/a2/a2_data.dart
+++ lib/data/levels/a2/a2_data.dart
@@ -1,1 +1,8 @@
-lib/data/levels/a2/a2_data.dart
+import 'a2_models.dart';
+import 'a2_lesson_01.dart';
+import 'a2_lesson_02.dart';
+
+const List<A2Lesson> a2Lessons = [
+  a2Lesson01,
+  a2Lesson02,
+];
