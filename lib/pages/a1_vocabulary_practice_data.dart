dbaa2330f49877ce9f35b9005638c7f9c5e945cb
Ari <ari934318@gmail.com>
2026-09-15 04:14:32 +0330

    Update a1_vocabulary_practice_data.dart

--- lib/pages/a1_vocabulary_practice_data.dart
+++ lib/pages/a1_vocabulary_practice_data.dart
@@ -1,5 +1,5 @@
-import 'a1_lessons.dart';
-import 'vocabulary_practice_models.dart';
+import '../data/levels/a1/a1_data.dart';
+import '../models/vocabulary_practice_models.dart';
 
 class A1VocabularyPracticeData {
   static List<VocabularyPracticeItem> get all {
